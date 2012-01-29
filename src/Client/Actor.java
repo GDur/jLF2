a005c74c2ef8b4be1b0ca54c9da9413edf938824
Marius <mariusbierlein@hotmail.de>
2012-01-29 15:54:23 +0100

    Actor.java

--- src/Client/Actor.java
+++ src/Client/Actor.java
@@ -16,23 +16,23 @@ public class Actor implements Comparable<Actor>
 		offset = 320;
 		tree = new AnimationTree();
 		b = Main.p.loadImage("sprite/" + name + ".png");
-		tree.add("standing", new Animation(b, new int[] { 0, 1, 2, 3 }, 80, 80, true));
-		tree.add("roll", new Animation(b, new int[] { 59, 58, 69, 4 }, 80, 80, false));
-		tree.add("walking", new Animation(b, new int[] { 4, 5, 6, 7 }, 80, 80, true));
-		tree.add("running", new Animation(b, new int[] { 20, 21, 22 }, 80, 80, true));
-		tree.add("punch", new Animation(b, new int[] { 10, 11, 12, 13, 14, 15, 16, 17 }, 80, 80, false));
-		tree.add("super_punch", new Animation(b, new int[] { 8, 9, 19, 29, 39, 49 }, 80, 80, false));
+		tree.add("standing"		, new Animation(b, new int[] { 0, 1, 2, 3 }						, 80, 80, true));
+		tree.add("roll"			, new Animation(b, new int[] { 59, 58, 69, 4 }					, 80, 80, false));
+		tree.add("walking"		, new Animation(b, new int[] { 4, 5, 6, 7 }						, 80, 80, true));
+		tree.add("running"		, new Animation(b, new int[] { 20, 21, 22 }						, 80, 80, true));
+		tree.add("punch"		, new Animation(b, new int[] { 10, 11, 12, 13, 14, 15, 16, 17 }	, 80, 80, false));
+		tree.add("super_punch"	, new Animation(b, new int[] { 8, 9, 19, 29, 39, 49 }			, 80, 80, false));
 
 	}
 
 	public void draw()
 	{
-		Main.p.image(tree.play("standing"), 80 * 0, 0);
-		Main.p.image(tree.play("walking"), 80 * 1, 0);
-		Main.p.image(tree.play("running"), 80 * 2, 0);
-		Main.p.image(tree.play("punch"), 80 * 3, 0);
-		Main.p.image(tree.play("super_punch"), 80 * 4, 0);
-		Main.p.image(tree.play("roll"), 80 * 5, 0);
+		Main.p.image(tree.play("standing")		, 80 * 0, 0);
+		Main.p.image(tree.play("walking")		, 80 * 1, 0);
+		Main.p.image(tree.play("running")		, 80 * 2, 0);
+		Main.p.image(tree.play("punch")			, 80 * 3, 0);
+		Main.p.image(tree.play("super_punch")	, 80 * 4, 0);
+		Main.p.image(tree.play("roll")			, 80 * 5, 0);
 	}
 
 	@Override
